ef58237f7c62a27b334bf0dbe4931e09f7303a37
tom <tom.martzolf@etu.unilim.fr>
2019-11-07 14:26:54 +0100

    page accueil

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,5 +1,9 @@
 <template>
-  <header>
-    <p> Salut ca marche bien </p>
-  </header>
+<body>
+  <button  type="button">  Acceuil de jour  </button>
+  <button  type="button">  Associations  </button>
+  <h1> Signaler quelque chose ?  </h1>  <button  type="button">  115  </button>
+  <p> Centres ouverts </p>
+  <div id="liste_asso"> </div>
+</body>
 </template>
