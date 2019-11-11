ec804dee648e1e1dfa1d605d7dc63b132bc45487
Clement Le Caloch <clement.le-caloch@etu.unilim.fr>
2019-11-11 21:57:44 +0100

    Lister Centre + css mieux

--- frontend/components/Header.vue
+++ frontend/components/Header.vue
@@ -1,27 +1,27 @@
 <template>
   <div>
-  <header>
-    <router-link tag="a" to="/" exact><h1>SIAO Limoges</h1></router-link>
-      <div class="choixLangue">
-        <div>
-        <label for="langue">Langue</label>
-        <select id="langue">
-          <option>Français</option>
-          <option>Anglais</option>
-          <option>Arabe</option>
-          <option>Russe</option>
-        </select>
+    <header>
+      <router-link tag="a" to="/" exact><h1>SIAO Limoges</h1></router-link>
+        <div class="choixLangue">
+          <div>
+          <label for="langue">Langue</label>
+          <select id="langue">
+            <option>Français</option>
+            <option>Anglais</option>
+            <option>Arabe</option>
+            <option>Russe</option>
+          </select>
+          </div>
+        <button>Connexion</button>
         </div>
-      <button>Connexion</button>
-      </div>
-  </header>
-  <nav>
-    <ul>
-      <li><router-link tag="a" to="/centre" exact>Accueil de jour</router-link></li>
-      <li><router-link tag="a" to="/association" exact>Association</router-link></li>
-    </ul>
-  <h2> Signaler quelque chose ?  </h2>
-  <button>  115  </button>
-  </nav>
+    </header>
+    <nav>
+      <ul>
+        <li><router-link tag="a" to="/centre" exact>Accueil de jour</router-link></li>
+        <li><router-link tag="a" to="/association" exact>Association</router-link></li>
+      </ul>
+    </nav>
+    <h2> Signaler quelque chose ?  </h2>
+    <button class="call">  115  </button>
   </div>
 </template>
--- frontend/pages/centre/index.vue
+++ frontend/pages/centre/index.vue
@@ -1,6 +1,55 @@
 <template>
   <div>
     <h3> Centres </h3>
-    <div id="liste_asso"> </div>
+    <div class="accueilDeJour">
+
+
+        <div class="listeCentre">
+
+          <div class="centre" v-for="centre in filteredList" v-bind:key="centre">
+            <h4>{{centre.association.nom}}</h4>
+            <h5>Adresse : </h5>
+            <p>{{centre.adresse}}</p>
+            <h5>Horraire d'ouverure : </h5>
+            <p>Matin : {{centre.heureOuvertureMatin}} - {{centre.heureFermetureMatin}}</p>
+            <p>Après-midi : {{centre.heureOuvertureSoir}} - {{centre.heureFermetureSoir}}</p>
+          </div>
+
+        </div>
+
+        <div class="map">
+            <h4> Les centres </h4>
+            <iframe src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=Limoges+(Les%20centres)&amp;ie=UTF8&amp;t=&amp;z=12&amp;iwloc=B&amp;output=embed">
+            </iframe>
+        </div>
+    </div>
   </div>
 </template>
+
+<script>
+import centreQuery from '~/apollo/queries/centre/centres'
+
+export default {
+  data() {
+    return {
+      centres: [],
+      query: ''
+    }
+  },
+  apollo: {
+    centres: {
+      prefetch: true,
+      query: centreQuery
+    }
+  },
+  computed: {
+    // Search system
+    filteredList() {
+      return this.centres.filter(centre => {
+        return centre.adresse.toLowerCase().includes(this.query.toLowerCase())
+      })
+    },
+  }
+}
+
+</script>
