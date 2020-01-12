713c932f801f92eccec00acbc8be76383bdc5afe
Dorian <dorianmare@yahoo.fr>
2020-01-12 20:10:20 +0100

    maj centres.gql sur nouveau Strapi

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,36 +1,40 @@
 <template>
-  <body>
-    <h3> Centres ouverts </h3>
-    <div class="accueilDeJour">
-      <div class="listeCentre">
-
-        <div class="centre" v-for="centre in filteredList" v-bind:key="centre">
-          <h4>{{centre.association.nom}}</h4>
-          <h5>Adresse : </h5>
-          <p>{{centre.adresse}}</p>
-          <h5>Horaire d'ouverture : </h5>
-          <p>Matin : {{centre.heureOuvertureMatin}} - {{centre.heureFermetureMatin}}</p>
-          <p>Après-midi : {{centre.heureOuvertureSoir}} - {{centre.heureFermetureSoir}}</p>
-        </div>
+<body>
+  <h3>Centres ouverts</h3>
+  <div class="accueilDeJour">
+    <div class="listeCentre">
+      <div class="centre" v-for="centre in filteredList" v-bind:key="centre">
+        <h4>{{centre.association.nom}}</h4>
+        <h5>Adresse :</h5>
+        <p>{{centre.adresse}}</p>
+        <h5>Horaire d'ouverture :</h5>
+        <p>Matin : {{centre.heureOuvertureMatin}} - {{centre.heureFermetureMatin}}</p>
+        <p>Après-midi : {{centre.heureOuvertureSoir}} - {{centre.heureFermetureSoir}}</p>
       </div>
-
-      <div class="map">
-          <h4> Les centres </h4>
-<iframe src="https://www.google.com/maps/d/u/0/embed?mid=10B3EB07xHA2NsGIkl3oS4JymcHtICPx6" width="640" height="480"></iframe>      </div>
     </div>
-  </body>
+
+    <div class="map">
+      <h4>Les centres</h4>
+      <iframe
+        src="https://www.google.com/maps/d/u/0/embed?mid=10B3EB07xHA2NsGIkl3oS4JymcHtICPx6"
+        width="640"
+        height="480"
+      ></iframe>
+    </div>
+  </div>
+</body>
 </template>
 
 
 <script>
-import centreQuery from '~/apollo/queries/centre/centres'
+import centreQuery from "~/apollo/queries/centre/centres";
 
 export default {
   data() {
     return {
       centres: [],
-      query: ''
-    }
+      query: ""
+    };
   },
   apollo: {
     centres: {
@@ -42,10 +46,9 @@ export default {
     // Search system
     filteredList() {
       return this.centres.filter(centre => {
-        return centre.adresse.toLowerCase().includes(this.query.toLowerCase())
-      })
-    },
+        return centre.adresse.toLowerCase().includes(this.query.toLowerCase());
+      });
+    }
   }
-}
-
+};
 </script>
