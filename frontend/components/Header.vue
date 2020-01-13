9b0517bef52257a5ae87b2767bd7e1efda65dbe8
Marion Brochet <marion.brochet@etu.unilim.fr>
2020-01-13 10:41:02 +0100

    Installation de la page de l'intra

--- frontend/components/Header.vue
+++ frontend/components/Header.vue
@@ -1,12 +1,22 @@
 <template>
   <div>
     <header>
-      <router-link tag="a" to="/" exact>
+      <div v-if="username">
+        <router-link tag="a" to="/intra" exact>
         <h1>
           SIAO 115
           <br />Haute-Vienne
         </h1>
       </router-link>
+      </div>
+      <div v-else>
+        <router-link tag="a" to="/" exact>
+        <h1>
+          SIAO 115
+          <br />Haute-Vienne
+        </h1>
+      </router-link>
+      </div>
       <div class="choixLangue">
         <div>
           <label for="langue">Langue</label>
@@ -14,25 +24,74 @@
         </div>
         <div v-if="username">
           <p>Vous êtes connecté en tant que {{ username }}</p>
-          <a href="#" @click="logout">Déconnexion</a>
+          <a href="/" @click="logout">Déconnexion</a>
         </div>
         <div v-else>
           <router-link tag="a" to="/connexion" exact>Connexion</router-link>
         </div>
       </div>
     </header>
-    <nav>
-      <ul>
-        <li>
-          <router-link tag="a" to="/centre" exact>Accueil de jour</router-link>
-        </li>
-        <li>
-          <router-link tag="a" to="/association" exact>Associations</router-link>
-        </li>
-      </ul>
-    </nav>
-    <h2>Signaler quelque chose ?</h2>
-    <button class="call">115</button>
+    <div v-if="username">
+      <nav>
+        <ul>
+          <li class="deroulant">
+            <router-link tag="a" to="/intra/MonAssociation" exact>Mon Association</router-link>
+          </li>
+          <li class="deroulant">
+            <router-link tag="a" to="/intra/MesCentres" exact>Mes Centres</router-link>
+            <ul class="sous">
+              <li class="sousligne"><router-link tag="a" to="/intra/MesCentres/AjouterCentre" exact>Ajouter Centre</router-link></li>
+              <li class="sousligne"><router-link tag="a" to="/intra/MesCentres/ModifierCentre" exact>Modifier Centre</router-link></li>
+              <li class="sousligne"><router-link tag="a" to="/intra/MesCentres/SupprimerCentre" exact>Supprimer Centre</router-link></li>
+            </ul>
+          </li>
+          <li class="deroulant">
+            <router-link tag="a" to="/intra/MesServices" exact>Mes Services</router-link>
+            <ul class="sous">
+              <li class="sousligne"><router-link tag="a" to="/intra/MesCentres/AjouterService" exact>Ajouter Service</router-link></li>
+              <li class="sousligne"><router-link tag="a" to="/intra/MesCentres/ModifierService" exact>Modifier Service</router-link></li>
+              <li class="sousligne"><router-link tag="a" to="/intra/MesCentres/SupprimerService" exact>Supprimer Service</router-link></li>
+            </ul>
+          </li>
+          <li class="deroulant">
+            <router-link tag="a" to="/intra/Maraudes" exact>Maraudes</router-link>
+            <ul class="sous">
+              <li class="sousligne"><router-link tag="a" to="/intra/MesCentres/AjouterMaraude" exact>Ajouter Maraude</router-link></li>
+              <li class="sousligne"><router-link tag="a" to="/intra/MesCentres/ModifierMaraude" exact>Modifier Maraude</router-link></li>
+              <li class="sousligne"><router-link tag="a" to="/intra/MesCentres/SupprimerMaraude" exact>Supprimer Maraude</router-link></li>
+              <li class="sousligne"><router-link tag="a" to="/intra/MesCentres/Rapport" exact>Rapport</router-link></li>
+            </ul>
+          </li>
+          <li class="deroulant">
+            <router-link tag="a" to="/intra/MesMembres" exact>Mes membres</router-link>
+            <ul class="sous">
+              <li class="sousligne"><router-link tag="a" to="/intra/MesCentres/AjouterMembre" exact>Ajouter Membre</router-link></li>
+              <li class="sousligne"><router-link tag="a" to="/intra/MesCentres/ModifierMembre" exact>Modifier Membre</router-link></li>
+              <li class="sousligne"><router-link tag="a" to="/intra/MesCentres/SupprimerMembre" exact>Supprimer Membre</router-link></li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    <div v-else>
+      <nav>
+        <ul>
+          <li>
+            <router-link tag="a" to="/centre" exact>Accueil de jour</router-link>
+          </li>
+          <li>
+            <router-link tag="a" to="/association" exact>Associations</router-link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    <div v-if="username">
+    </div>
+    <div v-else>
+      <h2>Signaler quelque chose ?</h2>
+      <button class="call">115</button>
+    </div>
+
   </div>
 </template>
 
---
+++ frontend/pages/intra/MonAssociation/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h3> Mon association </h3>
+
+    <div class="association" v-for="association in filteredList" v-bind:key="association">
+       <img :src="'http://localhost:1337/' + association.logo.url">
+       <div class="infoAssociation">
+          <h4>{{association.nom}}</h4>
+          <section>{{association.description}}</section>
+       </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import associationsQuery from '~/apollo/queries/association/associations'
+
+export default {
+  data() {
+    return {
+      associations: [],
+      query: ''
+    }
+  },
+  apollo: {
+    associations: {
+      prefetch: true,
+      query: associationsQuery
+    }
+  },
+  computed: {
+    // Search system
+    filteredList() {
+      return this.associations.filter(association => {
+        return association.nom.toLowerCase().includes(this.query.toLowerCase())
+      })
+    },
+  }
+}
+</script>
