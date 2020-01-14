ee3bd89f8ec62095785d409bfa133be77be273f2
tom <tom.martzolf@etu.unilim.fr>
2020-01-14 10:26:40 +0100

    Detail association et lien centre

--- frontend/pages/association/_id.vue
+++ frontend/pages/association/_id.vue
@@ -8,6 +8,12 @@
     <h3> Centres : </h3>
       <div class="association" v-for="centres in association.centres" v-bind:key="association.centres.id">
           <h3>{{centres.adresse}}</h3>
+          <p> Services proposés par le centre : </p>
+          <div class="services" v-for="services in centres.services" v-bind:key="centres.services.id">
+            <p> {{services.nom}} </p>
+        </div>
+        <br>
+        <router-link class="plusinfo":to="{ name: 'centre-id', params: { id: centres.id }}" tag="a" > Plus d'informations </router-link>
       </div>
   </client-only>
 </div>
