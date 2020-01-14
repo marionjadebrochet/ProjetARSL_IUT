8d3422a9e1196402c9cf45b4de97f7f1e8c1838a
GuillaumeRslt <guillaume.rousselet11@gmail.com>
2020-01-14 11:24:04 +0100

    mis en forme ajout dans intra

---
+++ frontend/pages/intra/MesCentres/AjouterCentre/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>Ajouter un centre</h1>
+        <form @submit.stop.prevent="ajouterCentre">
+            <label for="adresse">Adresse du centre :</label>
+            <input type="text" v-model="adresse"><br /><br />
+            <label for="telephone">Téléphone du centre :</label>
+            <input type="tel" v-model="telephone"><br /><br />
+            <!--check box pour animaux autorsé ou non-->
+            <label for="animaux"> Animaux :</label>
+            <input type="radio" v-model="accepte" checked> Autorisés
+  	        <input type="radio" v-model="refuse"> Interdits<br /><br />
+            <label for="public">public concerné par le centre :</label>
+            <input type="text" v-model="public"><br /><br />
+          <button type="submit">Ajouter</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        ajouterCentre() {
+
+        }
+    }
+}
+</script>
---
+++ frontend/pages/intra/MesServices/AjouterService/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Ajouter un service</h1>
+        <form @submit.stop.prevent="ajouterService">
+            <label for="nom">Nom du service:</label>
+            <input type="text" v-model="nom"><br /><br />
+            <label for="description">Description du centre :</label>
+            <input type="text" v-model="description"><br /><br />
+          <button type="submit">Ajouter</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        ajouterService() {
+
+        }
+    }
+}
+</script>
