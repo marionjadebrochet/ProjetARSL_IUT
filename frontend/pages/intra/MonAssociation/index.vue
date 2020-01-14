445ec31edbc6b8985f5495f27cec85ca55d42404
Clement Le Caloch <clement.le-caloch@etu.unilim.fr>
2020-01-14 15:47:28 +0100

    css partenaires

--- frontend/pages/intra/MonAssociation/index.vue
+++ frontend/pages/intra/MonAssociation/index.vue
@@ -11,15 +11,14 @@
     </div>
 
     <h3 style="padding-top:20px;"> Mes partenaires </h3><br>
-    <div class="partenaires" v-for="partenaire in association.partenaires" :key="association.partenaires.id">
-      <img :src="'http://localhost:1337/' + partenaire.logo.url">
-      <div class="infoPartenaire">
-        <h3> Nom du partenaire : </h3>
-        <h2>{{partenaire.nom}}</h2>
-        <h3> Téléphone du partenaire : </h3>
-        <h2>{{partenaire.telephone}}</h2>
-        <h3> Email du partenaire : </h3>
-        <h2>{{partenaire.email}}</h2></br></br>
+    <div class="allpartenaires">
+      <div class="partenaires" v-for="partenaire in association.partenaires" :key="association.partenaires.id">
+        <img :src="'http://localhost:1337/' + partenaire.logo.url">
+        <div class="infoPartenaire">
+          <h2>{{partenaire.nom}}</h2>
+          <p> Téléphone du partenaire : <b>{{partenaire.telephone}}</b></p>
+          <p> Email du partenaire : <b>{{partenaire.email}}</b> </p>
+        </div>
       </div>
     </div>
     <div>
@@ -57,3 +56,32 @@ export default {
   }
 }
 </script>
+
+<style>
+.allpartenaires {
+  display:flex;
+  flex-direction:row;
+  justify-content:space-around;
+  flex-wrap:wrap;
+}
+.partenaires {
+  width:40%;
+  display:flex;
+  flex-direction:row;
+  justify-content:space-around;
+  margin-top: 20px;
+  background-color:#EEEEEE;
+  padding:20px 20px;
+  border-radius:20px;
+  box-shadow:5px 5px 10px #CCCCCC;
+  margin-bottom:50px;
+}
+.partenaires > img {
+  width:auto;
+  height:300px;
+}
+.infoPartenaire > p {
+  padding:10px;
+  margin-top:10px;
+}
+</style>
