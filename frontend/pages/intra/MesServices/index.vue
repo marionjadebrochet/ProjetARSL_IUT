c2801468ff3bfb7ee2052bde182507b5963c5050
Marion Brochet <marion.brochet@etu.unilim.fr>
2020-01-14 16:05:44 +0100

    AJout page Mes services dans l'intra

---
+++ frontend/pages/intra/MesServices/index.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <client-only>
+      <h3 style="padding-top:20px;"> Mes Services </h3><br>
+
+      <div class="centre" v-for="centre in association.centres" v-bind:key="association.centres.id">
+      <div class="services" v-for="service in centre.services" :key="centre.services.id">
+        <div class="infoService">
+          <h3> Nom du service : </h3>
+          <h2>{{service.nom}}</h2>
+          <h3> Description du service : </h3>
+          <h2>{{service.description}}</h2>
+          <h3>Horaires d'ouverture :</h3>
+          <table>
+            <tr>
+              <td>Lundi </td>
+              <td>{{service.jourshoraires.lundiMatin}} </td>
+              <td>{{service.jourshoraires.lundiApresMidi}}</td>
+            </tr>
+            <tr>
+              <td>Mardi </td>
+              <td>{{service.jourshoraires.mardiMatin}} </td>
+              <td>{{service.jourshoraires.mardinApresMidi}}</td>
+            </tr>
+            <tr>
+              <td>Mercredi </td>
+              <td>{{service.jourshoraires.mercrediMatin}} </td>
+              <td>{{service.jourshoraires.mercrediApresMidi}}</td>
+            </tr>
+            <tr>
+              <td>Jeudi </td>
+              <td>{{service.jourshoraires.jeudiMatin}} </td>
+              <td>{{service.jourshoraires.jeudiApresMidi}}</td>
+            </tr>
+            <tr>
+              <td>Vendredi </td>
+              <td>{{service.jourshoraires.vendrediMatin}} </td>
+              <td>{{service.jourshoraires.vendrediApresMidi}}</td>
+            </tr>
+            <tr>
+              <td>Samedi </td>
+              <td>{{service.jourshoraires.samediMatin}} </td>
+              <td>{{service.jourshoraires.samediApresMidi}}</td>
+            </tr>
+            <tr>
+              <td>Dimanche </td>
+              <td>{{service.jourshoraires.dimancheMatin}} </td>
+              <td>{{service.jourshoraires.dimancheApresMidi}}</td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    </div>
+    </client-only>
+  </div>
+</template>
+
+<script>
+import associationQuery from '~/apollo/queries/association/association'
+
+export default {
+  data() {
+    return {
+      association: Object,
+      centre: Object,
+      services: [],
+      query: '',
+    }
+  },
+  computed: {
+    // Get your association thanks to your getter
+    associationUser() {
+      return this.$store.getters["auth/association"];
+    }
+  },
+  apollo: {
+    association: {
+      prefetch: true,
+      query: associationQuery,
+      variables () {
+        return { id: this.associationUser.id }
+      }
+    }
+  }
+}
+</script>
