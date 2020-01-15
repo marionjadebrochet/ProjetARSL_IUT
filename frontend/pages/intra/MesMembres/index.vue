0ee4d3e8c4dba4871ad96ce683c0bfc2dbd48ab7
Clement Le Caloch <clement.le-caloch@etu.unilim.fr>
2020-01-15 09:11:02 +0100

    css mes membres

--- frontend/pages/intra/MesMembres/index.vue
+++ frontend/pages/intra/MesMembres/index.vue
@@ -2,20 +2,34 @@
   <div>
     <client-only>
       <h3 style="padding-top:20px;"> Mes Utilisateurs </h3><br>
-      <div class="mesUtilisateurs">
-        <div v-for="user in association.users" v-bind:key="association.users.id">
-          <h3>Nom : </h3>
-          <p>{{user.Nom}}</p>
-          <h3>Pseudo : </h3>
-          <p>{{user.usernames}}</p>
-          <h3>Prenom : </h3>
-          <p>{{user.Prenom}}</p>
-          <h3>Telephone : </h3>
-          <p>{{user.Telephone}}</p>
-          <h3>Email : </h3>
-          <p>{{user.email}}</p>
-          <h3>Role : </h3>
-          <p>{{user.role.name}}</p>
+      <div class="center">
+        <div style="width:50%;" v-for="user in association.users" v-bind:key="association.users.id">
+          <div class="cart">
+            <div class="row">
+              <h3>Nom : </h3>
+              <p>{{user.Nom}}</p>
+            </div>
+            <div class="row">
+              <h3>Pseudo : </h3>
+              <p>{{user.usernames}}</p>
+            </div>
+            <div class="row">
+              <h3>Prenom : </h3>
+              <p>{{user.Prenom}}</p>
+            </div>
+            <div class="row">
+              <h3>Telephone : </h3>
+              <p>{{user.Telephone}}</p>
+            </div>
+            <div class="row">
+              <h3>Email : </h3>
+              <p>{{user.email}}</p>
+            </div>
+            <div class="row">
+              <h3>Role : </h3>
+              <p>{{user.role.name}}</p>
+            </div>
+          </div>
         </div>
       </div>
     </client-only>
