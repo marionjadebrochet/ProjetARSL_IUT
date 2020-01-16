137680bbb9f38092333abd31d4f264017d83e2f0
tom <tom.martzolf@etu.unilim.fr>
2020-01-16 09:02:46 +0100

    Pages de services (Ou manger ect ... ) + modif query services + changements valeures strapi

--- frontend/pages/services/manger/index.vue
+++ frontend/pages/services/manger/index.vue
@@ -1,5 +1,67 @@
 <template>
   <div>
-    <h3> Manger </h3>
+    <h2> Centres où manger </h2>
+
+    <h3> Petit déjeuner </h3>
+    <div class="services" v-for="service in filteredList" v-bind:key="service.nom">
+      <div v-if="service.nom=='Petit déjeuner'">
+               <h3> {{service.centre.adresse}} </h3>
+               <router-link class="plusinfo":to="{ name: 'centre-id', params: { id: service.centre.id }}" tag="a" > Plus d'informations </router-link>
+      </div>
+    </div>
+
+    <h3> Collation </h3>
+    <div class="services" v-for="service in filteredList" v-bind:key="service.nom">
+      <div v-if="service.nom=='Collation'">
+               <h3> {{service.centre.adresse}} </h3>
+               <router-link class="plusinfo":to="{ name: 'centre-id', params: { id: service.centre.id }}" tag="a" > Plus d'informations </router-link>
+      </div>
+    </div>
+
+    <div class="map">
+        <div id="map-wrap" style="height: 600px; ">
+           <l-map :zoom=12 :center="[45.835425,1.2644847]">
+             <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"> </l-tile-layer>
+                <div class="marker" v-for="service in filteredList" v-bind:key="service.centre.id">
+                  <div v-if="service.nom=='Collation'">
+                     <l-marker :lat-lng="[service.centre.latitude, service.centre.longitude]" >
+                     <l-popup :content="service.centre.association.nom + ' | ' + service.centre.adresse"/> </l-marker>
+                   </div>
+                   <div v-if="service.nom=='Petit déjeuner'">
+                      <l-marker :lat-lng="[service.centre.latitude, service.centre.longitude]" >
+                      <l-popup :content="service.centre.association.nom + ' | ' + service.centre.adresse"/> </l-marker>
+                    </div>
+                </div>
+           </l-map>
+        </div>
+    </div>
   </div>
 </template>
+
+
+<script>
+import servicesQuery from '~/apollo/queries/service/services'
+
+export default {
+  data() {
+    return {
+      services: [],
+      query: ''
+    }
+  },
+  apollo: {
+    services: {
+      prefetch: true,
+      query: servicesQuery
+    }
+  },
+  computed: {
+    // Search system
+    filteredList() {
+      return this.services.filter(services => {
+        return services.nom.toLowerCase().includes(this.query.toLowerCase())
+      })
+    },
+  }
+}
+</script>
