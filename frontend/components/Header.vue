465eff47c431599ffcb93458082fdd7605832350
Clement Le Caloch <clement.le-caloch@etu.unilim.fr>
2020-01-24 15:42:27 +0100

    hamburger

--- frontend/components/Header.vue
+++ frontend/components/Header.vue
@@ -2,28 +2,28 @@
   <div>
     <header>
         <router-link tag="a" to="/" exact><img class="logo" src="~/assets/image/logo.png"></router-link>
-        <div v-show="!username">
-          <div id="google_translate_element"></div>
-        </div>
+        <router-link tag="a" to="/" exact><h1>SIAO 115 Haute-Vienne</h1></router-link>
+        <div v-if="!username" id="google_translate_element"></div>
         <nav>
-          <ul v-if="!username">
-            <li>
-              <router-link tag="a" to="/association" exact>Associations</router-link>
-            </li>
-            <li>
-                <router-link tag="a" to="/maraudes" exact>Maraudes</router-link>
-            </li>
-            <li>
-                <router-link tag="a" to="/services" exact>Services</router-link>
-            </li>
-          </ul>
-          <div v-if="username" class="deco">
-              <p>{{ username }}</p>
-              <a class="orangeButton" href="/" @click="logout">Déconnexion</a>
-          </div>
-          <div v-else>
-              <router-link class="orangeButton" tag="a" to="/connexion" exact>Connexion</router-link>
-          </div>
+          <label>
+            <input type="checkbox" id="check">
+            <span class="menu">
+              <span class="hamburger"></span>
+            </span>
+            <ul>
+              <li v-on:click="uncheck"><router-link tag="a" to="/" exact>Accueil</router-link></li>
+              <li v-if="!username" v-on:click="uncheck"><router-link tag="a" to="/association" exact>Associations</router-link></li>
+              <li v-if="!username" v-on:click="uncheck"><router-link tag="a" to="/maraudes" exact>Maraudes</router-link></li>
+              <li v-if="!username" v-on:click="uncheck"><router-link tag="a" to="/services" exact>Services</router-link></li>
+              <div v-if="username" class="deco">
+                  <p>{{ username }}</p>
+                  <a v-on:click="uncheck" class="orangeButton" href="/" @click="logout">Déconnexion</a>
+              </div>
+              <div v-else>
+                  <a v-on:click="uncheck" class="orangeButton" href="/connexion" exact>Connexion</a>
+              </div>
+            </ul>
+          </label>
         </nav>
     </header>
   </div>
@@ -64,6 +64,11 @@ export default {
         id
       );
     },
+    uncheck: function() {
+        console.log("ca marche")
+        var item = document.getElementById('check');
+        item.checked = false;
+    },
 
     // Define your needed mutations, here: auth/logout
     ...mapMutations({
@@ -72,3 +77,104 @@ export default {
   }
 };
 </script>
+
+<style>
+label .menu {
+  position: absolute;
+  right:-100px;
+  top:-100px;
+  z-index: 100;
+  width: 200px;
+  height: 200px;
+  background-color: #E95133;
+  border-radius: 50%;
+  box-shadow: 0 0 0 0 white, 0 0 0 0 white;
+  cursor:pointer;
+  transition: 0.5s ease-in-out;
+}
+
+label .hamburger {
+  position: absolute;
+  left: 50px;
+  top:135px;
+  width:30px;
+  height: 2px;
+  background-color: white;
+  display: block;
+  transform-origin: center;
+}
+
+label .hamburger:after, label .hamburger:before {
+  content:"";
+  position: absolute;
+  display: block;
+  width: 100%;
+  height: 100% ;
+  background-color:white;
+}
+
+label .hamburger:before {
+  top: -10px;
+}
+label .hamburger:after {
+  bottom: -10px;
+}
+label input {
+  display: none;
+}
+
+label input:checked + .menu {
+  box-shadow: 0 0 0 120vw white, 0 0 0 100vh white;
+  border-radius: 30px;
+}
+label input:checked + .menu .hamburger {
+  transform:rotate(45deg);
+}
+
+label input:checked + .menu .hamburger:after {
+  transform:rotate(90deg);
+  bottom:0;
+}
+
+label input:checked + .menu .hamburger:before {
+  transform:rotate(90deg);
+  top:0;
+}
+
+label input:checked + .menu + ul {
+  visibility:visible;
+  opacity:1;
+}
+
+label ul {
+  text-align:center;
+  visibility:hidden;
+  opacity:0;
+  z-index: 100;
+  position: absolute;
+  right:50%;
+  transform: translate(50%, 50%);
+  transition: 0.4s 0s ease-in-out;
+}
+
+label a {
+  width:200px;
+  margin-bottom:10px;
+  font-size:18px;
+  display: block;
+  color:#000;
+  text-decoration: none;
+  text-transform: uppercase;
+  text-align: center;
+  padding:24px 36px;
+  transition: 0.1s ease-in;
+  letter-spacing:2px;
+}
+label a:hover {
+  font-size:24px;
+}
+label .orangeButton {
+  margin:auto;
+}
+
+</style>
